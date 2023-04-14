d15666523297027516685abf42a1558734881527
canberkkkkk <83513116+canberkkkkk@users.noreply.github.com>
2023-04-14 17:47:09 +0300

    feat(code): add problem

---
+++ LeetCode704.java
@@ -0,0 +1,18 @@
+public class LeetCode704 {
+  public int search(int[] nums, int target) {
+    int lo = 0;
+    int hi = nums.length - 1;
+
+    while (lo <= hi) {
+      int mid = (lo + hi) / 2;
+      if (target == nums[mid])
+        return mid;
+      else if (target < nums[mid])
+        hi = mid - 1;
+      else if (target > nums[mid])
+        lo = mid + 1;
+    }
+
+    return -1;
+  }
+}
