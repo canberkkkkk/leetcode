f57cbbf6701020851757088681f342216e3c33c8
canberkkkkk <83513116+canberkkkkk@users.noreply.github.com>
2023-04-21 08:11:39 +0300

    feat(code): add problem

---
+++ LeetCode1342.java
@@ -0,0 +1,15 @@
+public class LeetCode1342 {
+  public int numberOfSteps(int num) {
+    int counter = 0;
+    while (num != 0) {
+      if (num % 2 == 0)
+        num /= 2;
+      else
+        num -= 1;
+
+      counter++;
+    }
+
+    return counter;
+  }
+}
