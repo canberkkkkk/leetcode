8e44413699358817cadba5fb14c3c0e0a73aa930
canberkkkkk <83513116+canberkkkkk@users.noreply.github.com>
2023-05-01 14:27:17 +0300

    feat(code): add problem

---
+++ LeetCode19.java
@@ -0,0 +1,19 @@
+public class LeetCode19 {
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode slow = head, fast = head;
+
+    for (int i = 0; i < n; i++)
+      fast = fast.next;
+
+    if (fast == null)
+      return head.next;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+    return head;
+  }
+}
